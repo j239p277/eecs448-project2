400901a85e2ded15b9b556e5dcd4d5b6342b9cf8
Jan Polzer <j239p277@ku.edu>
2017-10-07 02:34:17 -0500

    Addded methods to Event

--- project-files/src/converge/Event.java
+++ project-files/src/converge/Event.java
@@ -1,7 +1,7 @@
 package converge;
 
-import java.io.*;
 import java.util.*;
+
 
 /**
  * The Event.java file is used to implement the Event class that stores the information needed
@@ -14,9 +14,9 @@ public class Event {
 	
 	String m_eventName = "";
 	String m_hostName = "";
-	Vector<Vector<String>> m_datesAndTimes = new Vector();
-	Vector<String> m_tasks = new Vector();
-	Vector<Vector<String>> m_attendees = new Vector();
+	Vector<Vector<String>> m_datesAndTimes = new Vector<Vector<String>>();
+	Vector<String> m_tasks = new Vector<String>();
+	Vector<Vector<String>> m_attendees = new Vector<Vector<String>>();
 	
 	public Event() {}
 	
@@ -47,6 +47,19 @@ public class Event {
 	
 	public Vector<Vector<String>> getAttendees() {
 		return m_attendees;
+	}
+	
+	public void addDatesAndTimes(Vector<String> temp) {
+		this.m_datesAndTimes.add(temp);
+	}
+	
+	public void addTask(String task)
+	{
+		this.m_tasks.add(task);
+	}
+	
+	public void addttendee(Vector<String> temp) {
+		this.m_attendees.add(temp);
 	}
 	
 	public void viewAllEvents() {
